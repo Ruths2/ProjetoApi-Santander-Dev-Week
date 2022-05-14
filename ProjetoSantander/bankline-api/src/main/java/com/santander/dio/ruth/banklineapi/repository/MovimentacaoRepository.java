f3debce625670f333109054bfe14e0e495e9dc51
Ruths2 <ruth09@yahoo.com>
2022-05-14 14:44:41 -0300

    Cria lista com as movimentações que sera requisitada pelo app atraves do IdConta e faz o push no Heroku

--- ProjetoSantander/bankline-api/src/main/java/com/santander/dio/ruth/banklineapi/repository/MovimentacaoRepository.java
+++ ProjetoSantander/bankline-api/src/main/java/com/santander/dio/ruth/banklineapi/repository/MovimentacaoRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.santander.dio.ruth.banklineapi.model.Movimentacao;
 
+import java.util.List;
+
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
+	public List<Movimentacao>findByIdConta(Integer idConta);
 }
+
